b5d705c27923842ae08fae164255efac2a420b87
Spandan Deb <spandan.deb04@gmail.com>
2025-05-25 14:51:05 +0530

    Added mongo db

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-from bson import ObjectId
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
